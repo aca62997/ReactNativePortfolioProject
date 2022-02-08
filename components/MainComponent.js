7989ff567700e98db746a1b16a60d208369ae465
Amanda Abrams <aca62997@gmail.com>
2022-02-08 16:55:04 -0500

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,39 +1,67 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import BakeryInfo from './BakeryInfoComponent';
 import Constants from 'expo-constants';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         BakeryInfo: { screen: BakeryInfo }
-    }, 
+    },
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
             headerStyle: {
                 backgroundColor: 'pink'
             },
-            headerTintColor: '#CE4165',
+            headerTintColor: '#ce4165',
             headerTitleStyle: {
-                color: 'CE4165'
+                color: '#ce4165'
             }
         }
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'pink'
+            },
+            headerTintColor: '#ce4165',
+            headerTitleStyle: {
+                color: '#ce4165'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Menu: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: 'pink'
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
             }}>
                 <AppNavigator />
             </View>
