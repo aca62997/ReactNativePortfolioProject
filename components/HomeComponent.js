9bc42704d9b50e81fefd7f6ae4d2d1d78b72f5ac
Amanda Abrams <aca62997@gmail.com>
2022-02-09 14:08:22 -0500

    Navigation Part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { BAKERIES } from '../shared/bakeries';
+
+function RenderItem({item}) {
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/sprinkles.jpg')}
+            >
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            bakeries: BAKERIES,
+        };
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,9 +34,11 @@ class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenderItem 
+                    item={this.state.bakeries.filter(bakery => bakery.featured)[0]}
+                />
+            </ScrollView>
         );
     }
 }
