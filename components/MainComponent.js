9dc2cbe7e5619ca8a6c6f96b61b897b83bf493f4
Amanda Abrams <aca62997@gmail.com>
2022-02-09 21:18:13 -0500

    Navigation Icons and Custom Side Drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,15 +5,26 @@ import BakeryInfo from './BakeryInfoComponent';
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Constants from 'expo-constants';
-import { View, Platform } from 'react-native';
+import { View, Platform, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: { screen: Directory },
-        BakeryInfo: { screen: BakeryInfo }
+      Directory: { 
+        screen: Directory,
+        navigationOptions: ({navigation}) => ({
+            headerLeft: <Icon
+                name='list'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+      },
+      BakeryInfo: { screen: BakeryInfo }
     },
     {
         initialRouteName: 'Directory',
@@ -34,50 +45,68 @@ const HomeNavigator = createStackNavigator(
         Home: { screen: Home }
     },
     {
-        defaultNavigationOptions: {
+        defaultNavigationOptions: ({navigation}) => ({
             headerStyle: {
                 backgroundColor: 'pink'
             },
             headerTintColor: '#ce4165',
             headerTitleStyle: {
                 color: '#ce4165'
-            }
-        }
+            },
+            headerLeft: <Icon
+                name='home'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
     }
 );
 
 const AboutNavigator = createStackNavigator(
-  {
-    About: { screen: About },
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "pink",
-      },
-      headerTintColor: "#ce4165",
-      headerTitleStyle: {
-        color: "#ce4165",
-      },
+    {
+        About: { screen: About }
     },
-  }
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: 'pink'
+            },
+            headerTintColor: '#ce4165',
+            headerTitleStyle: {
+                color: '#ce4165'
+            },
+            headerLeft: <Icon
+                name='info-circle'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
 );
 
 const ContactNavigator = createStackNavigator(
-  {
-    Contact: { screen: Contact },
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "pink",
-      },
-      headerTintColor: "#ce4165",
-      headerTitleStyle: {
-        color: "#ce4165",
-      },
+    {
+        Contact: { screen: Contact }
     },
-  }
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: 'pink'
+            },
+            headerTintColor: '#ce4165',
+            headerTitleStyle: {
+                color: '#ce4165'
+            },
+            headerLeft: <Icon
+                name='address-card'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
 );
 
 const MainNavigator = createDrawerNavigator(
@@ -106,5 +135,13 @@ class Main extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    stackIcon: {
+        marginLeft: 10,
+        color: '#fff',
+        fontSize: 24
+    }
+});
 
 export default Main;
